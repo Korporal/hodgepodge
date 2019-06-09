a7c3d3a249b92a5b448bfef1cce5f0f3514ff674
Hugh <hugh_gleaves@hotmail.com>
2019-06-09 09:55:43 -0700

    Cleanup usings.

--- Locking/ConsoleApp18/NativeCriticalSections/Program.cs
+++ Locking/ConsoleApp18/NativeCriticalSections/Program.cs
@@ -1,10 +1,4 @@
-﻿using CriticalSections;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 
 namespace ConsoleApp18
 {
