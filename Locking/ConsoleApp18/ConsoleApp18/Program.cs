01af3057645e6403a90bffbb1636c32875b7a27c
Hugh <hugh_gleaves@hotmail.com>
2019-06-09 09:42:45 -0700

    Added example of using native Win32 Critical Sections.

---
+++ Locking/ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,71 @@
+﻿using CriticalSections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            SomeObject obj = new SomeObject();
+
+            Thread t1 = new Thread(ThreadProc);
+            Thread t2 = new Thread(ThreadProc);
+
+            t1.Start(obj);
+            t2.Start(obj);
+
+            Thread.Sleep(100000);
+
+        }
+
+        public static void ThreadProc(object instance)
+        {
+            SomeObject obj = (SomeObject)(instance);
+
+            for (int I=0; I < 10000; I++)
+            {
+                obj.ChangeMyState();
+                Thread.Sleep(1);
+            }
+        }
+
+    }
+
+    public abstract unsafe class ThreadSafeObject
+    {
+        private CRITICAL_SECTION section;
+
+        protected ThreadSafeObject()
+        {
+            fixed (CRITICAL_SECTION* section_ptr = &section) { Native.InitializeCriticalSection(section_ptr); }
+        }
+
+        protected void Lock()
+        {
+            fixed (CRITICAL_SECTION* section_ptr = &section) { Native.EnterCriticalSection(section_ptr); }
+        }
+
+        protected void Unlock()
+        {
+            fixed (CRITICAL_SECTION* section_ptr = &section) { Native.LeaveCriticalSection(section_ptr); }
+        }
+    }
+
+    public class SomeObject : ThreadSafeObject
+    {
+        public int count = 0;
+
+        public void ChangeMyState()
+        {
+            //Lock();
+            count++;
+            //Unlock();
+        }
+    }
+}
---
+++ Locking/ConsoleApp18/CriticalSections/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalSections
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct CRITICAL_SECTION
+    {
+        public IntPtr DebugInfo;
+        public int LockCount;
+        public int RecursionCount;
+        public IntPtr OwningThread;
+        public IntPtr LockSemaphore;
+        public IntPtr SpinCount;
+    }
+
+    public unsafe static class Native
+    {
+        [DllImport("kernel32")]
+        public static extern void InitializeCriticalSection(CRITICAL_SECTION* CriticalSection);
+
+        [DllImport("kernel32")]
+        public static extern void EnterCriticalSection(CRITICAL_SECTION* CriticalSection);
+
+        [DllImport("kernel32")]
+        public static extern void LeaveCriticalSection(CRITICAL_SECTION* CriticalSection);
+
+    }
+}
